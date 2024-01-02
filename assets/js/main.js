091760813bc9f23f9e1adab7b1268e08e9107002
maksymberezhnoy <maksymberezhnoy@gmail.com>
2024-01-02 23:34:43 +0200

    add QTY plus/minus buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function ($) {
-    $("body").on("updated_checkout", function () {
+    const body = $('body');
+
+    body.on("updated_checkout", function () {
         $.ajax({
             url: mb_localize.admin_url,
             type: "POST",
@@ -16,6 +18,56 @@ jQuery(document).ready(function ($) {
             }
         });
     });
+
+
+    //QTY box
+    if (!String.prototype.getDecimals) {
+        String.prototype.getDecimals = function () {
+            let num = this,
+                match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
+            if (!match) {
+                return 0;
+            }
+            return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
+        }
+    }
+
+    //QTY box buttons (minus, plus)
+    body.on('click', '.qty_plus__js, .qty_minus__js', function (e) {
+        e.preventDefault();
+
+        let $qty = $(this).closest('.quantity').find('input[type="number"]'),
+            currentVal = parseFloat($qty.val()),
+            max = parseFloat($qty.attr('max')),
+            min = parseFloat($qty.attr('min')),
+            step = $qty.attr('step');
+
+        // Format values
+        if (!currentVal || currentVal === '' || currentVal === 'NaN') currentVal = 0;
+        if (max === '' || max === 'NaN') max = '';
+        if (min === '' || min === 'NaN') min = 0;
+        if (step === 'any' || step === '' || step === undefined || parseFloat(step) === 'NaN') step = '1';
+
+        // Change the value
+        if ($(this).is('.qty_plus__js')) {
+            if (max && (currentVal >= max)) {
+                $qty.val(max);
+            } else {
+                $qty.val((currentVal + parseFloat(step)).toFixed(step.getDecimals()));
+            }
+        } else {
+            if (min && (currentVal <= min)) {
+                $qty.val(min);
+            } else if (currentVal > 0) {
+                $qty.val((currentVal - parseFloat(step)).toFixed(step.getDecimals()));
+            }
+        }
+
+        // Trigger change event
+        $qty.trigger('change');
+    });
+
+
 });
 
 /* Menu Slider
--- inc/checkout.php
+++ inc/checkout.php
@@ -3,3 +3,70 @@
 add_action( 'woocommerce_review_order_before_payment', function () { ?>
 	<h3><?php esc_html_e( 'Оплата', 'woocommerce' ); ?></h3>
 <?php } );
+
+/**
+ * Change default Woo form input[type=number] HTML
+ */
+function mb_change_woocommerce_form_field_number_html( $field, $key, $args, $value ) {
+
+	//change CSS class to allow JS usage for the input[type=number]
+	if ( str_contains( $field, 'woocommerce-input-wrapper' ) ) {
+		$field = str_replace( 'woocommerce-input-wrapper', 'woocommerce-input-wrapper__mb_js_allowed', $field );
+	}
+
+	$input_start = '<input type=';
+	$input_end   = '/>';
+
+	$minus = mb_display_minus_quantity_input_button( true );
+	$plus  = mb_display_plus_quantity_input_button( true );
+
+	$start_position = strpos( $field, $input_start );
+	$end_position   = strpos( $field, $input_end );
+
+	if ( $start_position !== false && $end_position !== false ) {
+		// Insert '-' before $input_start
+		$field = substr_replace( $field, $minus, $start_position, 0 );
+
+		// Insert '+' after $input_end
+		$field = substr_replace( $field, $plus, $end_position + strlen( $minus ) + strlen( $input_end ), 0 );
+	}
+
+	return $field;
+
+}
+
+add_filter( 'woocommerce_form_field_number', 'mb_change_woocommerce_form_field_number_html', 10, 4 );
+
+/**
+ * Display MINUS quantity input button
+ */
+function mb_display_minus_quantity_input_button( $return = false ) {
+	if ( $return ) {
+		ob_start();
+	}
+
+	echo '<span class="qty-button mb-qty-minus qty_minus__js">-</span>';
+
+	if ( $return ) {
+		return ob_get_clean();
+	}
+}
+
+add_action( 'woocommerce_before_quantity_input_field', 'mb_display_minus_quantity_input_button' );
+
+
+/**
+ * Display PLUS quantity input button
+ */
+function mb_display_plus_quantity_input_button( $return = false ) {
+	if ( $return ) {
+		ob_start();
+	}
+	echo '<span class="qty-button mb-qty-plus qty_plus__js">+</span>';
+
+	if ( $return ) {
+		return ob_get_clean();
+	}
+}
+
+add_action( 'woocommerce_after_quantity_input_field', 'mb_display_plus_quantity_input_button' );
